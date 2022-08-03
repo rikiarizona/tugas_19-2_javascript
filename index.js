c8aa7243e97376d18fd7a359df71af8d4cc31e76
rikiarizona <rikiarizona@gmail.com>
2022-08-03 10:10:03 +0700

    TUGAS 19-2 JAVASCRIPT
    saya kirimkan tugas niomic

---
+++ index.js
@@ -0,0 +1,12 @@
+function repeatBintang() {
+    console.log('*'.repeat(5));
+    console.log('*****'.repeat(5));
+    console.log('**********'.repeat(5));
+    console.log('***************'.repeat(5));
+    console.log('********************'.repeat(5));
+    console.log('***************'.repeat(5));
+    console.log('**********'.repeat(5));
+    console.log('*****'.repeat(5));
+    console.log('*'.repeat(5));
+}
+repeatBintang();
